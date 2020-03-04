ae778cb53fe993127bfbab2dcacc40531867529e
maikia <maja_ka@hotmail.com>
2020-03-04 11:15:46 +0100

    flake8

--- simulate.py
+++ simulate.py
@@ -1,5 +1,4 @@
 import os.path as op
-import os
 import random
 
 import numpy as np
@@ -35,31 +34,32 @@ if hemi == ('both' or 'rh'):
 def make_random_parcellation(path_annot, n, hemi, subjects_dir, random_state,
                              subject):
     parcel = random_parcellation(subject, n, hemi, subjects_dir=subjects_dir,
-                             surface='white', random_state=random_state)
+                                 surface='white', random_state=random_state)
 
     mne.write_labels_to_annot(parcel, subjects_dir=subjects_dir,
                               subject=subject,
                               annot_fname=path_annot,
                               overwrite=True)
 
+
 def find_centers_of_mass(parcellation, subjects_dir):
     centers = np.zeros([len(parcellation)])
     # calculate center of mass for the labels
     for idx, parcel in enumerate(parcellation):
         centers[idx] = parcel.center_of_mass(restrict_vertices=True,
-                                            surf='white',
-                                            subjects_dir=subjects_dir)
+                                             surf='white',
+                                             subjects_dir=subjects_dir)
     return centers.astype('int')
 
 
 # check if the annotation already exists, if not create it
 if hemi == ('both' or 'lh') and not op.exists(random_annot_path_lh):
-    make_random_parcellation(random_annot_path_lh, n, 'lh', subjects_dir, random_state,
-                         subject)
+    make_random_parcellation(random_annot_path_lh, n, 'lh', subjects_dir,
+                             random_state, subject)
 
 if hemi == ('both' or 'lr') and not op.exists(random_annot_path_rh):
-    make_random_parcellation(random_annot_path_rh, n, 'rh', subjects_dir, random_state,
-                         subject)
+    make_random_parcellation(random_annot_path_rh, n, 'rh', subjects_dir,
+                             random_state, subject)
 
 # read the labels from annot
 if hemi == ('both' or 'lh'):
@@ -80,7 +80,7 @@ if hemi == ('both' or 'rh'):
 # and exact parcels
 n_parcels = random.randint(1, 1)
 if hemi == 'both':
-    hemi_selected = random.choices(['lh', 'rh'], weights = [1, 1])[0]
+    hemi_selected = random.choices(['lh', 'rh'], weights=[1, 1])[0]
 else:
     hemi_selected = hemi
 parcel_selected = random.randint(0, n-1)
@@ -94,6 +94,7 @@ elif hemi_selected == 'rh':
     l1_center_of_mass.vertices = [cm_rh[parcel_selected]]
     parcel_used = parcels_rh[parcel_selected]
 
+
 def generate_signal(data_path, subject, parcel):
     # Generate the signal
     # First, we get an info structure from the test subject.
@@ -104,8 +105,8 @@ def generate_signal(data_path, subject, parcel):
     info = mne.pick_info(info, sel)
     tstep = 1. / info['sfreq']
 
-    # To simulate sources, we also need a source space. It can be obtained from the
-    # forward solution of the sample subject.
+    # To simulate sources, we also need a source space. It can be obtained from
+    # the forward solution of the sample subject.
     fwd_fname = op.join(data_path, 'MEG', subject,
                         subject+'_audvis-meg-eeg-oct-6-fwd.fif')
     fwd = mne.read_forward_solution(fwd_fname)
@@ -114,11 +115,12 @@ def generate_signal(data_path, subject, parcel):
     # Define the time course of the activity for each source of the region to
     # activate. Here we use a sine wave at 18 Hz with a peak amplitude
     # of 10 nAm.
-    source_time_series = np.sin(2. * np.pi * 18. * np.arange(100) * tstep) * 10e-9
+    source_time_series = np.sin(2. * np.pi * 18. * np.arange(100) * tstep
+                                ) * 10e-9
 
-    # Define when the activity occurs using events. The first column is the sample
-    # of the event, the second is not used, and the third is the event id. Here the
-    # events occur every 200 samples.
+    # Define when the activity occurs using events. The first column is the
+    # sample of the event, the second is not used, and the third is the event
+    # id. Here the events occur every 200 samples.
     n_events = 50
     events = np.zeros((n_events, 3))
     events[:, 0] = 100 + 200 * np.arange(n_events)  # Events sample.
@@ -130,12 +132,14 @@ def generate_signal(data_path, subject, parcel):
     source_simulator = mne.simulation.SourceSimulator(src, tstep=tstep)
     source_simulator.add_data(parcel, source_time_series, events)
 
-    # Project the source time series to sensor space and add some noise. The source
-    # simulator can be given directly to the simulate_raw function.
+    # Project the source time series to sensor space and add some noise.
+    # The source simulator can be given directly to the simulate_raw function.
     raw = mne.simulation.simulate_raw(info, source_simulator, forward=fwd)
     cov = mne.make_ad_hoc_cov(raw.info)
     mne.simulation.add_noise(raw, cov, iir_filter=[0.2, -0.2, 0.04])
+
     return events, source_time_series, raw
+
 
 events, source_time_series, raw = generate_signal(data_path, subject,
                                                   parcel=l1_center_of_mass)
@@ -154,15 +158,11 @@ brain = Brain('sample', hemi, 'inflated', subjects_dir=subjects_dir,
 
 brain.add_label(parcel_used, alpha=0.5, color='r')
 # 0 if lh, 1 if rh
-l = mne.vertex_to_mni(l1_center_of_mass.vertices, 0, subject, subjects_dir)
+# l = mne.vertex_to_mni(l1_center_of_mass.vertices, 0, subject, subjects_dir)
 
-
-#brain.add_foci(l, map_surface="white", color="gold")
-#brain.add_foci(label1_center_of_mass, coords_as_verts=True,
-#map_surface="white", color="red")
 for center in cm_lh:
-    brain.add_foci(center, coords_as_verts=True,
-                    map_surface="white", color="gold", hemi=hemi_selected)
+    brain.add_foci(center, coords_as_verts=True, map_surface="white",
+                   color="gold", hemi=hemi_selected)
 
 file_save_brain = 'fig/brain.png'
 brain.save_image(file_save_brain)
