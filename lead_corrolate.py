824c18270a5fcb4f452f09499663d3a4c37fefdd
maikia <maja_ka@hotmail.com>
2020-03-19 10:28:30 +0100

    making new estimator using lead matrix colleralation

---
+++ lead_corrolate.py
@@ -0,0 +1,5 @@
+# to check if estimator works for sklearn do:
+# from sklearn.utils.estimator_checks import check_estimator
+# from sklearn.svm import LinearSVC
+# check_estimator(LinearSVC)  # passes
+
