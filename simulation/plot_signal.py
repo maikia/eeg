106ab5dba5422c3997c0f094a52013702df4d83f
Alexandre Gramfort <alexandre.gramfort@m4x.org>
2020-03-25 14:15:24 +0100

    plot brain

--- simulation/plot_signal.py
+++ simulation/plot_signal.py
@@ -16,21 +16,6 @@ def visualize_brain(subject, hemi, annot_name, subjects_dir, parcels_selected):
                   cortex='low_contrast', background='white', size=(800, 600))
 
     for parcel in parcels_selected:
-        brain.add_label(parcel, alpha=1, color='r')
-    # brain.add_label(parcels_rh[0], alpha=0.5, color='b')
-    # brain.add_label(corpus_callosum, alpha=0.5, color='b')
-    # brain.add_label(parcels_rh[0], alpha=0.5, color='b')
-    # 0 if lh, 1 if rh
-    # l = mne.vertex_to_mni(l1_center_of_mass.vertices, 0,subject,subjects_dir)
-    if hemi == 'lh' or hemi == 'both':
-        brain.add_annotation(annot_name, borders=True, color='r',
-                             alpha=0.2)
-    if hemi == 'rh' or hemi == 'both':
-        brain.add_annotation(annot_name, borders=True, color='r',
-                             alpha=0.2)
-    # for center in cm_lh:
-    #    brain.add_foci(center, coords_as_verts=True, map_surface="white",
-    #                   color="gold", hemi=hemi_selected)
+        brain.add_label(parcel, alpha=1, color=parcel.color)
 
-    file_save_brain = 'fig/brain.png'
-    brain.save_image(file_save_brain)
+    brain.save_image('fig/brain.png')
