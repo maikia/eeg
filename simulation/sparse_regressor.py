bac8c46b28e46722dc06021e073707194a693959
Alexandre Gramfort <alexandre.gramfort@m4x.org>
2020-06-09 16:38:26 +0200

    make reweighting work

--- simulation/sparse_regressor.py
+++ simulation/sparse_regressor.py
@@ -1,13 +1,99 @@
+import warnings
+
 import numpy as np
 import pandas as pd
 
-from sklearn.base import BaseEstimator, ClassifierMixin, TransformerMixin
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.base import RegressorMixin, ClassifierMixin
 from sklearn.exceptions import ConvergenceWarning
 from sklearn.multioutput import MultiOutputRegressor
-import warnings
+from sklearn import linear_model
 
 from sklearn.metrics import hamming_loss
 # from simulation.emd import emd_score
+
+
+def solver_lasso(Xw, y, alpha, max_iter):
+    model = linear_model.LassoLars(max_iter=max_iter, normalize=False,
+                                   fit_intercept=False, alpha=alpha)
+    return model.fit(Xw, y).coef_.copy()
+
+
+
+class ReweightedLasso(BaseEstimator, RegressorMixin):
+    """ Reweighted Lasso estimator with L1 regularizer.
+
+    The optimization objective for Reweighted Lasso is::
+        (1 / (2 * n_samples)) * ||Y - XW||^2_Fro + alpha * ||W||_0.5
+
+    Where::
+        ||W||_0.5 = sum_i sum_j sqrt|w_ij|
+
+    Parameters
+    ----------
+    alpha : (float or array-like), shape (n_tasks)
+        Optional, default ones(n_tasks)
+        Constant that multiplies the L0.5 term. Defaults to 1.0
+    max_iter : int, optional
+        The maximum number of inner loop iterations
+    max_iter_reweighting : int, optional
+        Maximum number of reweighting steps i.e outer loop iterations
+    tol : float, optional
+        The tolerance for the optimization: if the updates are
+        smaller than ``tol``, the optimization code checks the
+        dual gap for optimality and continues until it is smaller
+        than ``tol``.
+
+    Attributes
+    ----------
+    coef_ : array, shape (n_features,)
+        Parameter vector (W in the cost function formula).
+    """
+    def __init__(self, alpha_fraction=.01, max_iter=2000,
+                 max_iter_reweighting=100, tol=1e-4):
+        self.alpha_fraction = alpha_fraction
+        self.max_iter = max_iter
+        self.max_iter_reweighting = max_iter_reweighting
+        self.tol = tol
+
+    def fit(self, X, y):
+        n_tasks = len(X)
+        n_samples, n_features = X.shape
+
+        self.coef_ = np.zeros(n_features)
+        weights = np.ones_like(self.coef_)
+        coef_old = self.coef_.copy()
+
+        self.loss_ = []
+
+        alpha_max = abs(X.T.dot(y)).max() / len(X)
+        alpha = self.alpha_fraction * alpha_max
+
+        for i in range(self.max_iter_reweighting):
+            Xw = X * weights
+            coef_ = solver_lasso(Xw, y, alpha, self.max_iter)
+            coef_ = coef_ * weights
+            err = abs(coef_ - coef_old).max()
+            err /= max(abs(coef_).max(), abs(coef_old).max(), 1.)
+            coef_old = coef_.copy()
+            weights = 2 * (abs(coef_) ** 0.5 + 1e-10)
+            obj = 0.5 * ((X @ coef_ - y) ** 2).sum() / n_samples
+            obj += (alpha * abs(coef_) ** 0.5).sum()
+            self.loss_.append(obj)
+            if err < self.tol and i:
+                break
+
+        if i == self.max_iter_reweighting - 1 and i:
+            warnings.warn('Reweighted objective did not converge.' +
+                          ' You might want to increase ' +
+                          'the number of iterations of reweighting.' +
+                          ' Fitting data with very small alpha' +
+                          ' may cause precision problems.',
+                          ConvergenceWarning)
+        self.coef_ = coef_
+
+    def predict(self, X):
+        return np.dot(X, self.coef_)
 
 
 def _get_coef(est):
@@ -17,15 +103,13 @@ def _get_coef(est):
 
 
 class SparseRegressor(BaseEstimator, ClassifierMixin, TransformerMixin):
-    def __init__(self, lead_field, parcel_indices, model, n_jobs=1,
-                 weighted_alpha=True):
+    def __init__(self, lead_field, parcel_indices, model, n_jobs=1):
         self.lead_field = lead_field
         self.parcel_indices = parcel_indices
         self.model = model
         self.n_jobs = n_jobs
         # self.data_dir = data_dir # this is required only if EMD score would
         # be used
-        self.weighted_alpha = weighted_alpha
 
     def fit(self, X, y):
         return self
@@ -58,81 +142,18 @@ class SparseRegressor(BaseEstimator, ClassifierMixin, TransformerMixin):
     def predict(self, X):
         return (self.decision_function(X) > 0).astype(int)
 
-    def residual(self, X, coef_, y):
-        """Compute y - X @ coef_."""
-        R = y - np.array([x.dot(th) for x, th in zip(X, coef_.T)])
-        return R
-
-    def _run_reweighted_model(self, model, L, X, max_iter_reweighting=100):
-        # weighted model
-        # adapted from https://github.com/hichamjanati/mutar
-
-        # TODO: how to correctly normalize?
-        norms = L.std(axis=0)
-        L = L / norms[None, :]
-
-        Xarr = np.array(X)
-        tol = 1e-4
-        n_tasks = len(X.index.values)
-        n_samples, n_features = L.shape
-        coef_ = np.zeros((n_tasks, n_features))
-
-        weights = np.ones(coef_.shape)  # np.ones_like(coef_[0, :])
-        coef_old = coef_.copy()
-
-        # TODO: calculate it for each L separately
-        alpha_max = abs(L.T.dot(X.T)).max() / len(L)
-        alpha = 0.05 * alpha_max
-        model.alpha = alpha
-        max_iter_reweighting = 2
-        # TODO: exchange ordering of the for loops (so less data in the mem)
-        for i in range(max_iter_reweighting):
-            Lw = L[None, :, :]
-            weights_ = weights[:, None, :]
-            Lw = Lw * weights_
-
-            # norms = Lw.std(axis=1)
-            # Lw = Lw / norms[:, None, :]
-
-            for ii in range(Xarr.shape[0]):
-                coef_[ii] = model.fit(Lw[ii, :, :], Xarr[ii, :]).coef_
-
-            coef_ = coef_ * weights
-
-            err = abs(coef_ - coef_old).max()
-            err /= max(abs(coef_).max(), abs(coef_old).max(), 1.)
-            print(err)
-            coef_old = coef_.copy()
-            weights = 2 * (abs(coef_) ** 0.5 + 1e-10)
-
-            if err < tol and i:
-                break
-        coef_ /= norms
-
-        if i == max_iter_reweighting - 1 and i:
-            warnings.warn('Reweighted objective did not converge.' +
-                          ' You might want to increase ' +
-                          'the number of iterations of reweighting.' +
-                          ' Fitting data with very small alpha' +
-                          ' may cause precision problems.',
-                          ConvergenceWarning)
-        return coef_.T
-
     def _run_model(self, model, L, X, fraction_alpha=0.2):
-        model = MultiOutputRegressor(model, n_jobs=self.n_jobs)
-        norms = L.std(axis=0)
+        norms = np.linalg.norm(L, axis=0)
         L = L / norms[None, :]
 
-        alpha_max = abs(L.T.dot(X.T)).max() / len(L)
-        alpha = fraction_alpha * alpha_max
-
-        model.estimator.alpha = alpha
-        model.fit(L, X.T)  # cross validation done here
         est_coefs = np.empty((X.shape[0], L.shape[1]))
         for idx, idx_used in enumerate(X.index.values):
-            est_coef = np.abs(_get_coef(model.estimators_[idx]))
+            x = X.iloc[idx].values
+            model.fit(L, x)
+            est_coef = np.abs(_get_coef(model))
             est_coef /= norms
             est_coefs[idx] = est_coef
+
         return est_coefs.T
 
     def decision_function(self, X):
@@ -146,11 +167,7 @@ class SparseRegressor(BaseEstimator, ClassifierMixin, TransformerMixin):
             X_used = X[X['subject_id'] == subj_idx]
             X_used = X_used.iloc[:, :-2]
 
-            if self.weighted_alpha:
-                est_coef = self._run_reweighted_model(self.model, l_used,
-                                                      X_used)
-            else:
-                est_coef = self._run_model(self.model, l_used, X_used)
+            est_coef = self._run_model(self.model, l_used, X_used)
 
             beta = pd.DataFrame(
                        np.abs(est_coef)
--- simulation/tests/test_sparse_regressor.py
+++ simulation/tests/test_sparse_regressor.py
@@ -6,16 +6,15 @@ import pandas as pd
 
 from sklearn import linear_model
 from sklearn.model_selection import cross_validate, train_test_split
+from sklearn.metrics import hamming_loss, jaccard_score
 
-from simulation.sparse_regressor import SparseRegressor
+from simulation.sparse_regressor import SparseRegressor, ReweightedLasso
 
 SEED = 42
 
 
 def make_dataset(n_subjects=1, n_samples_per_subj=2, n_parcels=10,
                  n_sources=300, n_sensors=100, max_true_sources=1):
-    # TODO: here each L will be of the same size, change so that sources_no
-    # varies between subjects
     rng = np.random.RandomState(SEED)
 
     electrode_names = [f'e{i}' for i in range(1, n_sensors + 1)]
@@ -24,24 +23,22 @@ def make_dataset(n_subjects=1, n_samples_per_subj=2, n_parcels=10,
     X = []
     y = []
 
-    # X, y, L, parcel_indices, signal_type_data
     for k in range(n_subjects):
         # make random Lead Field
-        Lk = rng.rand(n_sensors, n_sources)
+        Lk = rng.randn(n_sensors, n_sources)
         L.append(Lk)
 
-        # make parcel_indices
-        parcel_indices_k = rng.randint(1, n_parcels+1, n_sources)
+        parcel_indices_k = (np.arange(n_sources) % n_parcels) + 1
         parcel_indices.append(parcel_indices_k)
 
         mask = np.zeros((n_samples_per_subj, n_sources), dtype=bool)
         yk = np.zeros((n_samples_per_subj, n_parcels), dtype=int)
         for i in range(n_samples_per_subj):
-            mask[i, rng.randint(0, n_sources, size=2)] = True
+            mask[i, rng.randint(0, n_sources, size=max_true_sources)] = True
             yk[i, np.unique(parcel_indices_k[mask[i]]) - 1] = 1
 
         beta = rng.randn(n_samples_per_subj, n_sources)
-        beta[mask] = 0
+        beta[~mask] = 0
 
         Xk = (Lk @ beta.T).T
 
@@ -57,19 +54,26 @@ def make_dataset(n_subjects=1, n_samples_per_subj=2, n_parcels=10,
     return X, y, L, parcel_indices
 
 
-@pytest.mark.parametrize('solver',
-                         ['lasso_lars'])
-def test_sparse_regressor(solver):
-    if solver == 'lasso_lars':
-        model = linear_model.LassoLars(max_iter=3, normalize=False,
-                                       fit_intercept=False)
+lasso = linear_model.LassoLars(max_iter=2, normalize=False,
+                               fit_intercept=False, alpha=0.01)
+rwl1 = ReweightedLasso(alpha_fraction=.01, max_iter=20,
+                       max_iter_reweighting=1, tol=1e-4)
+rwl10 = ReweightedLasso(alpha_fraction=.01, max_iter=20,
+                       max_iter_reweighting=10, tol=1e-4)
 
+
+@pytest.mark.parametrize('model, hl_max',
+                         [(lasso, 0.02),
+                          (rwl1, 0),
+                          (rwl10, 0.002)
+                          ])
+def test_sparse_regressor(model, hl_max):
     n_subjects = 1
-    n_samples_per_subj = 2
+    n_samples_per_subj = 100
     n_parcels = 10
-    n_sources = 300
+    n_sources = 500
     n_sensors = 100
-    max_true_sources = 1
+    max_true_sources = 2
     X, y, L, parcel_indices = make_dataset(
         n_subjects, n_samples_per_subj, n_parcels, n_sources,
         n_sensors, max_true_sources
@@ -83,11 +87,12 @@ def test_sparse_regressor(solver):
     assert len(L) == n_subjects == len(parcel_indices)
     assert L[0].shape == (n_sensors, n_sources)
     assert y.shape[1] == n_parcels
+    assert np.mean(y.sum(1) == max_true_sources) > 0.9
 
-    sparse_regressor = SparseRegressor(L, parcel_indices, model)
+    sparse_regressor = SparseRegressor(
+        L, parcel_indices, model
+    )
 
-    X_train, X_test, y_train, y_test = \
-        train_test_split(X, y, test_size=0.2, random_state=42)
-
-    sparse_regressor.fit(X_train, y_train)
-    score = sparse_regressor.score(X_test, y_test)
+    y_pred = sparse_regressor.predict(X)
+    hl = hamming_loss(y_pred, y)
+    assert hl <= hl_max
