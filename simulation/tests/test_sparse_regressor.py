c66dd1cb76553f655f883d929062df9158e3bdeb
Alexandre Gramfort <alexandre.gramfort@m4x.org>
2020-06-11 22:00:25 +0200

    make test pass

--- simulation/tests/test_sparse_regressor.py
+++ simulation/tests/test_sparse_regressor.py
@@ -20,6 +20,8 @@ def make_dataset_from_sample():
     # assign paths
     data_path = mne.datasets.sample.data_path()
     subjects_dir = os.path.join(data_path, 'subjects')
+    mne.datasets.fetch_hcp_mmp_parcellation(subjects_dir=subjects_dir,
+                                            verbose=True)
 
     # get parcels and remove corpus callosum
     parcels = mne.read_labels_from_annot('fsaverage', 'HCPMMP1_combined',
@@ -58,12 +60,7 @@ def make_dataset_from_sample():
             to_remove.append(idx)
 
     # morph from fsaverage to sample
-    parcels = mne.morph_labels(parcels, 'sample', 'fsaverage', subjects_dir,
-                               'white')
-
-    subjects_dir = mne.datasets.sample.data_path() + '/subjects'
-    mne.datasets.fetch_hcp_mmp_parcellation(subjects_dir=subjects_dir,
-                                            verbose=True)
+    parcels = mne.morph_labels(parcels, 'sample', subjects_dir=subjects_dir)
 
     raw_fname = os.path.join(data_path, 'MEG', 'sample',
                              'sample_audvis_raw.fif')
@@ -74,6 +71,8 @@ def make_dataset_from_sample():
 
     info = mne.io.read_info(raw_fname)
     sel = mne.pick_types(info, meg='grad', eeg=False, stim=True, exclude=[])
+    sel_data = mne.pick_types(info, meg='grad', eeg=False, stim=False, exclude=[])
+    info_data = mne.pick_info(info, sel_data)
     info = mne.pick_info(info, sel)
     tstep = 1. / info['sfreq']
 
@@ -81,11 +80,16 @@ def make_dataset_from_sample():
     fwd = mne.read_forward_solution(fwd_fname)
     src = fwd['src']
 
+    fwd = mne.convert_forward_solution(fwd, force_fixed=True)
+    fwd = mne.pick_channels_forward(fwd, include=info_data['ch_names'], ordered=True)
+    lead_field = fwd['sol']['data']
+
     rng = np.random.RandomState(42)
 
-    n_samples = 2
-    signal_len = 10
+    n_samples = 3
+    signal_len = 20
     n_events = 50
+    n_events = 2
     add_noise = False
     source_time_series = np.sin(2. * np.pi *
                                 18. * np.arange(signal_len) * tstep) * 10e-9
@@ -98,8 +102,14 @@ def make_dataset_from_sample():
     true_idx = np.empty(n_samples, dtype=np.int16)
     for idx, source in enumerate(range(n_samples)):
         idx_source = rng.choice(np.arange(len(parcels)))
-        true_idx[idx] = int(idx_source)
+        true_idx[idx] = idx_source
         source = parcels[idx_source]
+
+        location = 'center'  # Use the center of the region as a seed.
+        extent = 0.
+        source = mne.label.select_sources(
+            'sample', source, location=location, extent=extent,
+            subjects_dir=subjects_dir)
 
         source_simulator = mne.simulation.SourceSimulator(src, tstep=tstep)
         source_simulator.add_data(source, source_time_series, events)
@@ -115,20 +125,15 @@ def make_dataset_from_sample():
         signal_list.append(data)
 
     signal_list = np.array(signal_list)
-    data_labels = ['e%d' % (idx + 1) for idx in range(signal_list.shape[1])]
+    # data_labels = [f'e{idx + 1}' for idx in range(signal_list.shape[1])]
+    data_labels = evoked.ch_names
 
     X = pd.DataFrame(signal_list, columns=list(data_labels))
     X['subject_id'] = 0
     X['subject'] = '0'
 
-    y = np.zeros((n_samples, len(parcels)))
+    y = np.zeros((n_samples, len(parcels)), dtype=int)
     y[np.arange(n_samples), true_idx] = 1
-
-    fwd = mne.convert_forward_solution(fwd, force_fixed=True)
-    lead_field = fwd['sol']['data']
-    picks_meg = mne.pick_types(fwd['info'], meg='grad',
-                               eeg=False, exclude=[])
-    lead_field = lead_field[picks_meg, :]
 
     parcel_vertices = {}
     for idx, parcel in enumerate(parcels, 1):
@@ -147,7 +152,7 @@ def make_dataset_from_sample():
 
     # Make sure label numbers different for each hemisphere
     parcel_indices = np.concatenate((parcel_indices_lh,
-                                    parcel_indices_rh), axis=0)
+                                     parcel_indices_rh), axis=0)
 
     # Now pick vertices that are actually used in the forward
     inuse = np.concatenate((fwd['src'][0]['inuse'],
@@ -247,12 +252,12 @@ def test_sparse_regressor(model, hl_max):
     assert hl <= hl_max
 
 
-@pytest.mark.parametrize('model, hl_max',
-                         [(lasso, 0.02),
-                          (rwl1, 0),
-                          (rwl10, 0.002)
+@pytest.mark.parametrize('model, hl_max, params',
+                         [(lasso, 0.0, {'alpha': 0.01}),
+                          (rwl1, 0.0, {'alpha_fraction': 0.1, 'max_iter': 1}),
+                          (rwl10, 0.0, {'alpha_fraction': 0.8})
                           ])
-def test_sparse_regressor_on_sample(make_dataset_from_sample, model, hl_max):
+def test_sparse_regressor_on_sample(make_dataset_from_sample, model, hl_max, params):
 
     X, y, L, parcel_indices = make_dataset_from_sample
     assert X.shape[0] == y.shape[0]
@@ -264,6 +269,10 @@ def test_sparse_regressor_on_sample(make_dataset_from_sample, model, hl_max):
     assert L[0].shape[1] == len(parcel_indices[0])  # n_sources
     assert y.shape[1] == len(np.unique(parcel_indices))  # n_parcels
 
+    L = 1e8 * np.array(L)
+    X.iloc[:, :-2] *=  1e12
+
+    model.set_params(**params)
     sparse_regressor = SparseRegressor(L, parcel_indices, model)
 
     y_pred = sparse_regressor.predict(X)
